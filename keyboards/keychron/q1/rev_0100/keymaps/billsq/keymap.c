5bdb048d135b4736a45da8c06c4855c6309e8e71
billsq <billsq@billsq.me>
2022-03-22 19:04:31 -0700

    Add custom keymap for Keychron Q1

---
+++ keyboards/keychron/q1/rev_0100/keymaps/billsq/keymap.c
@@ -0,0 +1,292 @@
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum layers{
+  MAC_BASE,
+  MAC_FN,
+  WIN_BASE,
+  WIN_FN
+};
+
+enum custom_keycodes {
+    KC_MISSION_CONTROL = USER00,
+    KC_LAUNCHPAD,
+    KC_TASK,
+    KC_FLXP
+};
+
+#define KC_MCTL KC_MISSION_CONTROL
+#define KC_LPAD KC_LAUNCHPAD
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [MAC_BASE] = LAYOUT_ansi_82(
+        KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,   KC_HOME,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_END,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI, MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [MAC_FN] = LAYOUT_ansi_82(
+        _______,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
+
+    [WIN_BASE] = LAYOUT_ansi_82(
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,   KC_HOME,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_END,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+    [WIN_FN] = LAYOUT_ansi_82(
+        _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______)
+};
+
+#ifdef RGB_MATRIX_ENABLE
+
+#ifndef RGB_CONFIRMATION_BLINKING_TIME
+#define RGB_CONFIRMATION_BLINKING_TIME 2000 // 2 seconds
+#endif
+
+/* Renaming those to make the purpose on this keymap clearer */
+#define LED_FLAG_CAPS LED_FLAG_NONE
+#define LED_FLAG_EFFECTS LED_FLAG_INDICATOR
+
+static void set_rgb_caps_leds(bool);
+
+#if RGB_CONFIRMATION_BLINKING_TIME > 0
+static uint16_t effect_started_time = 0;
+static uint8_t r_effect = 0x0, g_effect = 0x0, b_effect = 0x0;
+
+/* The higher this is, the slower the blinking will be */
+#ifndef TIME_SELECTED_BIT
+    #define TIME_SELECTED_BIT 8
+#endif
+#if TIME_SELECTED_BIT < 0 || TIME_SELECTED_BIT >= 16
+    #error "TIME_SELECTED_BIT must be a positive integer smaller than 16"
+#endif
+#define effect_red() r_effect = 0xFF, g_effect = 0x0, b_effect = 0x0
+#define effect_green() r_effect = 0x0, g_effect = 0xFF, b_effect = 0x0
+#endif // RGB_CONFIRMATION_BLINKING_TIME > 0
+
+bool led_update_user(led_t led_state) {
+    if (led_state.caps_lock) {
+        if (!rgb_matrix_is_enabled()) {
+            /* Turn ON the RGB Matrix for CAPS LOCK */
+            rgb_matrix_set_flags(LED_FLAG_CAPS);
+            rgb_matrix_enable();
+        }
+    } else if (rgb_matrix_get_flags() == LED_FLAG_CAPS) {
+        /* RGB Matrix was only ON because of CAPS LOCK. Turn it OFF. */
+        rgb_matrix_set_flags(LED_FLAG_ALL);
+        rgb_matrix_disable();
+    }
+    return true;
+}
+
+void rgb_matrix_indicators_user() {
+    #if RGB_CONFIRMATION_BLINKING_TIME > 0
+    if (effect_started_time > 0) {
+        /* Render blinking EFFECTS */
+        uint16_t deltaTime = sync_timer_elapsed(effect_started_time);
+        if (deltaTime <= RGB_CONFIRMATION_BLINKING_TIME) {
+            uint8_t led_state = ((~deltaTime) >> TIME_SELECTED_BIT) & 0x01;
+            uint8_t val_r = led_state * r_effect;
+            uint8_t val_g = led_state * g_effect;
+            uint8_t val_b = led_state * b_effect;
+            rgb_matrix_set_color_all(val_r, val_g, val_b);
+            set_rgb_caps_leds(host_keyboard_led_state().caps_lock);
+            return;
+        } else {
+            /* EFFECTS duration is finished */
+            effect_started_time = 0;
+            if (rgb_matrix_get_flags() == LED_FLAG_EFFECTS) {
+                /* It was turned ON because of EFFECTS */
+                if (host_keyboard_led_state().caps_lock) {
+                    /* CAPS is still ON. Demote to CAPS */
+                    rgb_matrix_set_flags(LED_FLAG_CAPS);
+                } else {
+                    /* There is nothing else keeping RGB enabled. Reset flags and turn if off. */
+                    rgb_matrix_set_flags(LED_FLAG_ALL);
+                    rgb_matrix_disable_noeeprom();
+                }
+            }
+        }
+    }
+    #endif // RGB_CONFIRMATION_BLINKING_TIME > 0
+    if (rgb_matrix_get_flags() == LED_FLAG_CAPS) {
+        rgb_matrix_set_color_all(0x0, 0x0, 0x0);
+    }
+    set_rgb_caps_leds(host_keyboard_led_state().caps_lock);
+}
+
+static void set_caps_indicator_leds(uint8_t red, uint8_t green, uint8_t blue)
+{
+    rgb_matrix_set_color(0, red, green, blue);
+    rgb_matrix_set_color(15, red, green, blue);
+    rgb_matrix_set_color(30, red, green, blue);
+    rgb_matrix_set_color(45, red, green, blue);
+    rgb_matrix_set_color(59, red, green, blue);
+    rgb_matrix_set_color(72, red, green, blue);
+}
+
+static void set_rgb_caps_leds(bool state) {
+    static bool old_state = 0;
+    static uint16_t caps_enabled_time = 0;
+    static bool blinker_state = 0;
+
+    if (state != old_state)
+    {
+        if (state)
+        {
+            caps_enabled_time = sync_timer_read();
+            blinker_state = 1;
+        }
+        else
+        {
+            caps_enabled_time = 0;
+            blinker_state = 0;
+        }
+    }
+
+    old_state = state;
+
+    if (state)
+    {
+        uint16_t deltaTime = sync_timer_elapsed(caps_enabled_time);
+
+        if ((deltaTime >> 9) > 0)
+        {
+            blinker_state = !blinker_state;
+            caps_enabled_time = sync_timer_read();
+        }
+
+        if (blinker_state)
+        {
+            set_caps_indicator_leds(0xFF, 0, 0);
+        }
+        else
+        {
+            set_caps_indicator_leds(0, 0, 0);
+        }
+    }
+}
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_MISSION_CONTROL:
+            if (record->event.pressed) {
+                host_consumer_send(0x29F);
+            } else {
+                host_consumer_send(0);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_LAUNCHPAD:
+            if (record->event.pressed) {
+                host_consumer_send(0x2A0);
+            } else {
+                host_consumer_send(0);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_TASK:
+            if (record->event.pressed) {
+                register_code(KC_LWIN);
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_LWIN);
+                unregister_code(KC_TAB);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_FLXP:
+            if (record->event.pressed) {
+                register_code(KC_LWIN);
+                register_code(KC_E);
+            } else {
+                unregister_code(KC_LWIN);
+                unregister_code(KC_E);
+            }
+            return false;  // Skip all further processing of this key
+#ifdef RGB_MATRIX_ENABLE
+        case RGB_MOD:
+        case RGB_RMOD:
+        case RGB_HUI:
+        case RGB_HUD:
+        case RGB_SAI:
+        case RGB_SAD:
+        case RGB_VAI:
+        case RGB_VAD:
+        case RGB_SPI:
+        case RGB_SPD:
+            if (record->event.pressed) {
+                if (rgb_matrix_get_flags() != LED_FLAG_ALL) {
+                    /* Ignore changes to RGB settings while only it's supposed to be OFF */
+                    return false;
+                }
+            }
+            break;
+        case RGB_TOG:
+            if (record->event.pressed) {
+                if (rgb_matrix_is_enabled()) {
+                    switch (rgb_matrix_get_flags()) {
+                        #if RGB_CONFIRMATION_BLINKING_TIME > 0
+                        case LED_FLAG_EFFECTS:
+                        #endif
+                        case LED_FLAG_CAPS:
+                            /* Turned ON because of EFFECTS or CAPS, is actually OFF */
+                            /* Change to LED_FLAG_ALL to signal it's really ON */
+                            rgb_matrix_set_flags(LED_FLAG_ALL);
+                            /* Will be re-enabled by the processing of the toggle */
+                            rgb_matrix_disable_noeeprom();
+                            break;
+                        case LED_FLAG_ALL:
+                            /* Is actually ON */
+                            #if RGB_CONFIRMATION_BLINKING_TIME > 0
+                            if (effect_started_time > 0) {
+                                /* Signal EFFECTS */
+                                rgb_matrix_set_flags(LED_FLAG_EFFECTS);
+                                /* Will be re-enabled by the processing of the toggle */
+                                rgb_matrix_disable_noeeprom();
+                            } else
+                            #endif
+                            if (host_keyboard_led_state().caps_lock) {
+                                /* Signal CAPS */
+                                rgb_matrix_set_flags(LED_FLAG_CAPS);
+                                /* Will be re-enabled by the processing of the toggle */
+                                rgb_matrix_disable_noeeprom();
+                            }
+                            break;
+                    }
+                }
+            }
+            break;
+#endif
+    }
+
+    return true;
+}
