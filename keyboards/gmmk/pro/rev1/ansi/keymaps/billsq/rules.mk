90a230d2d70e2fc5ab0d9333b1adb5bf1c9dcfca
billsq <billsq@billsq.me>
2022-04-19 22:15:49 -0700

    Use asym_eager debounce type, 8ms debounce time.

--- keyboards/gmmk/pro/rev1/ansi/keymaps/billsq/rules.mk
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/billsq/rules.mk
@@ -1,6 +1,8 @@
 #LTO_ENABLE = yes
 VIA_ENABLE = yes
-#DEBOUNCE_TYPE = sym_defer_pk
 ENCODER_ENABLE = yes
 # Disabling MouseKey
 MOUSEKEY_ENABLE = no
+
+#DEBOUNCE_TYPE = sym_defer_pk
+DEBOUNCE_TYPE = asym_eager_defer_pk
